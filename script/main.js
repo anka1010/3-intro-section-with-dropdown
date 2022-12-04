7e19d3a38f82feddc637724da3363d47763cdcac
anka1010 <an.louc@gmail.com>
2022-12-04 18:03:08 +0300

    Totally refactor the whole project

--- script/main.js
+++ script/main.js
@@ -1,14 +1,13 @@
-const menu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuClose = document.querySelector('.mobile-menu-close');
-const mobileFadeBlock = document.querySelector('.mobile-fade-block');
+const nav = document.querySelector('#nav');
+const mobileMenuBtn = document.querySelector('#mobile-menu-btn');
+const mobileMenuBtnImg = document.querySelector('#mobile-menu-btn img');
+const overlay = document.querySelector('#overlay');
 
-mobileMenu.addEventListener('click', () => {
-  mobileFadeBlock.classList.remove('hidden');
-  menu.classList.remove('hidden');
-});
-
-mobileMenuClose.addEventListener('click', () => {
-  menu.classList.add('hidden');
-  mobileFadeBlock.classList.add('hidden');
+mobileMenuBtn.addEventListener('click', () => {
+  if (nav.classList.toggle('open')) {
+    mobileMenuBtnImg.src = '../img/icons/icon-close-menu.svg';
+  } else {
+    mobileMenuBtnImg.src = '../img/icons/icon-menu.svg';
+  }
+  overlay.classList.toggle('open');
 });
