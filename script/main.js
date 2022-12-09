58b238474d53c03749949474762d7cd3a81a5d1a
anka1010 <an.louc@gmail.com>
2022-12-09 08:55:16 +0300

    Minor changes

--- script/main.js
+++ script/main.js
@@ -4,10 +4,11 @@ const mobileMenuBtnImg = document.querySelector('#mobile-menu-btn img');
 const overlay = document.querySelector('#overlay');
 
 mobileMenuBtn.addEventListener('click', () => {
-  if (nav.classList.toggle('open')) {
-    mobileMenuBtnImg.src = 'img/icons/icon-close-menu.svg';
-  } else {
-    mobileMenuBtnImg.src = 'img/icons/icon-menu.svg';
-  }
-  overlay.classList.toggle('open');
+   // MOBILE MENU
+   if (nav.classList.toggle('open')) {
+      mobileMenuBtnImg.src = 'img/icons/icon-close-menu.svg';
+   } else {
+      mobileMenuBtnImg.src = 'img/icons/icon-menu.svg';
+   }
+   overlay.classList.toggle('open');
 });
